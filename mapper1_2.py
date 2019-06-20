9fc1d5412904479a49a34dea6b3c1c989f074b7d
wzding <emma.ding@airbnb.com>
2019-06-20 15:50:39 -0700

    changed output types

---
+++ mapper1_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""mapper.py for question 1"""
+
+import sys
+
+Carrier = 8
+ArrDelay = 38
+
+def mapper():
+	for line in sys.stdin:
+		try:
+			line = line.replace('"', '').split(",")
+		except ValueError:
+			continue
+		if len(line) < 78:
+			continue
+		print("%s\t%s" % (line[Carrier], line[ArrDelay]))
+
+if __name__ == "__main__":
+	mapper()
