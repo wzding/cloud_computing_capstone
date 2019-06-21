4102cec26bfce3b87d5cce9a3ec1ff61d9c905bb
wzding <emma.ding@airbnb.com>
2019-06-21 11:03:45 -0700

    added reducer file

---
+++ reducer2_1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""reducer.py for question 1.2"""
+
+import sys
+
+def reducer():
+
+	curr_aprt = None
+	curr_carrier = None
+	total_delay = 0
+	count = 0
+
+	for line in sys.stdin:
+		aprt, carrier, delay = line.strip().split("\t")
+
+		try:
+			delay = float(delay)
+		except ValueError:
+			continue
+		# same airport and same carrier
+		if curr_aprt == aprt and curr_carrier == carrier:
+			total_delay += delay 
+			count += 1
+		# different carrier or diff aprt
+		else:
+			if curr_aprt and curr_carrier:
+				print("%s\t%s\t%.2f" % (
+				      curr_aprt, curr_carrier, total_delay * 1.0/ count)) 
+			curr_aprt = aprt
+			curr_carrier = carrier
+			total_delay = delay 
+			count = 1
+	# last aprt and carrier
+	if curr_aprt == aprt and curr_carrier == carrier:
+		print("%s\t%s\t%.2f" % (
+			  curr_aprt, curr_carrier, total_delay * 1.0/ count)) 
+
+if __name__ == "__main__":
+	reducer()
+		
